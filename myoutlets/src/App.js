ee5e2fef220c1f9fa49fd8bfb375d878b1033651
adautofbn <barros.adauto@gmail.com>
2018-10-29 17:15:23 -0300

    learning react with vtex

--- myoutlets/src/App.js
+++ myoutlets/src/App.js
@@ -1,28 +1,36 @@
-/* eslint-disable react/prefer-stateless-function */
+import React, { Component, Fragment } from "react";
 
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Login from "./Login";
+import Minicart from "./Minicart";
+import Gallery from "./components/Gallery";
+import mockData from "./data.json";
 
 class App extends Component {
+  handleChange = e => {
+    // TODO: implementar pesquisa
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <div className="flex flex-wrap-s flex-nowrap-ns justify-around items-center ph5-ns ph3-s pv8-ns pv3-s bb b--muted-4 mb4">
+          <input
+            className="ba b--muted-4 bw1 br5 pv4 ph6 f5 c-on-base bg-base order-2-s order-1-ns w-100-s w5-ns"
+            placeholder="Buscar"
+            onChange={this.handleChange}
+          />
+
+          <div className="c-muted-2 order-1-s order-2-ns ml-auto ml0-m mv3-s mv0-ns">
+            <div className="mh5 dib pointer">
+              <Login />
+            </div>
+            <div className="mh5 dib pointer">
+              <Minicart />
+            </div>
+          </div>
+        </div>
+        <Gallery products={mockData} />
+      </Fragment>
     );
   }
 }
